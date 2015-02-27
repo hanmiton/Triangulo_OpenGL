45a840ef2779d7eaca9f3f57eda4c1eddfe6b700
Fabricio Gavilanes <Fabricio3220@hotmail.com>
2015-02-26 21:39:52 -0500

    Modificado Fabricio Gavilanes Comentario

---
+++ app/src/androidTest/java/com/darwin/user/triogl/MainActivityTest.java
@@ -0,0 +1,26 @@
+package com.darwin.user.triogl;
+
+import junit.framework.TestCase;
+
+public class MainActivityTest extends TestCase {
+
+    public void testOnCreate() throws Exception {
+
+    }
+
+    public void testOnPause() throws Exception {
+
+    }
+
+    public void testOnStop() throws Exception {
+
+    }
+
+    public void testOnCreateOptionsMenu() throws Exception {
+
+    }
+
+    public void testOnOptionsItemSelected() throws Exception {
+
+    }
+}
--- app/src/main/java/com/darwin/user/triogl/MainActivity.java
+++ app/src/main/java/com/darwin/user/triogl/MainActivity.java
@@ -1,12 +1,11 @@
 package com.darwin.user.triogl;
 
-import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.opengl.GLSurfaceView;
-
+//Comentado por Fabricio Gavilanes mediante GitHub
 public class MainActivity extends ActionBarActivity {
     GLSurfaceView vista;
     @Override
